31c51c2cb229ec3cdbc4df2c96299b0bac088313
AndyKuhorenko <zfesandydaytona@gmail.com>
2022-09-20 16:15:15 +0300

    test: tests for pluralization and interpolation by data attributes added

--- __test__/test-dom.js
+++ __test__/test-dom.js
@@ -11,4 +11,14 @@ function testDom(customAttr) {
     `;
 }
 
-export default testDom;
+function interpolationTestDom(customAttr) {
+    const attr = customAttr || ATTRIBUTE_NAME;
+
+    return `
+        <h1 ${attr}="HelloName" ${attr}-name="John" ${attr}-surname="Doe"></h1>
+        <span ${attr}="Apples" ${attr}-name="apples" ${attr}-plural="4"></span>
+        <span ${attr}="Apples" ${attr}-name="oranges" ${attr}-plural="6"></span>
+    `;
+}
+
+export { testDom, interpolationTestDom };
